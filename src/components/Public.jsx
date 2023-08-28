55c220387c9affc9b3db6dd986c25da8cb08b29f
Your Name <ljweaver@umich.edu>
2023-08-28 19:56:20 -0400

    refactored App.js to use routes, organized file structure

---
+++ src/components/Public.jsx
@@ -0,0 +1,10 @@
+import ScheduleValidator from "../schedule_validator/components/ScheduleValidator"
+
+const Public = () => {
+  return (
+    <main className="main">
+         <ScheduleValidator />
+    </main>
+  )
+}
+export default Public
